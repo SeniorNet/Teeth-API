27ccd5660c839f5f6de2bf0b5b4404839f3443a4
SeniorNet <109102047+SeniorNet@users.noreply.github.com>
2022-08-30 15:02:12 -0400

    Final version

--- Web-API/Web-API/Program.cs
+++ Web-API/Web-API/Program.cs
@@ -1,6 +1,13 @@
+using Web_API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DbManagement>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
